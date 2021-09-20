e6181223b2f22114fc732c697c288e7a58c5ab58
KiSEiGitHub <disfigurehollah@gmail.com>
2021-09-20 18:30:30 +0200

    b = c ? | savoir si un nombre est divisible par 29

--- Fiche_4/Exercice_1.py
+++ Fiche_4/Exercice_1.py
@@ -12,5 +12,12 @@ def main():
     print(f'b = {b}')
     print(f'c = {c}')
 
+    print('-' * 50)
+
+    print(f'29 / 565 = {bool(29 % 565)}')
+    print(f'29 / 6785 = {bool(29 % 6785)}')
+    print(f'29 / 646195034 = {bool(29 % 646195034)}')
+    print(f'29 / 1970659794 = {bool(29 % 1970659794)}')
+
 if __name__ == '__main__':
     main()

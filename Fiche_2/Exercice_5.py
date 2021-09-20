4d040074bb87de4573c3a75eeade896e2e33e4d6
KiSEiGitHub <disfigurehollah@gmail.com>
2021-09-20 17:50:31 +0200

    Quotient et reste

---
+++ Fiche_2/Exercice_5.py
@@ -0,0 +1,8 @@
+def main():
+    rzefa = int(input('n: '))
+    fedrg = int(input('n: '))
+    print(f'Reste = {rzefa % fedrg}')
+    print(f'Quotient = {rzefa // fedrg}')
+
+if __name__ == '__main__':
+    main()

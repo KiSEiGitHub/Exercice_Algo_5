af9a342ebc4221dca0e7cc4e7f737181028b35d1
KiSEiGitHub <disfigurehollah@gmail.com>
2021-09-21 11:12:03 +0200

    La suite de Fibonacci

--- Fiche_5/Exercice_3/Exercice_3.py
+++ Fiche_5/Exercice_3/Exercice_3.py
@@ -1,13 +1,19 @@
 def main():
+    n = 6
     A = 1
     B = 1
 
-    for _ in range(11):
+    while n <= 10:
         C = B
         B = A + B
         A = C
         print(B)
+        n += 1
 
+    print('-' * 50)
+    print(f'A: {A}')
+    print(f'B: {B}')
+    print(f'C: {C}')
 
 if __name__ == '__main__':
     main()

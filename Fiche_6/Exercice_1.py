88f06e8ff0ca0e2512ac2343191634b9d782bf75
KiSEiGitHub <disfigurehollah@gmail.com>
2021-09-21 11:27:28 +0200

    Plusieurs boucles

---
+++ Fiche_6/Exercice_1.py
@@ -0,0 +1,32 @@
+class multiple_boucle:
+
+    @staticmethod
+    def boucle_1():
+        for i in range(10):
+            print(i)
+
+    @staticmethod
+    def boucle_2():
+        for n in range(16, 0, -1):
+            print(n)
+
+    @staticmethod
+    def boucle_3():
+        for i in range(18, 45):
+            if i % 2 == 0:
+                print(i)
+
+def main():
+    print('Boucle 1')
+    multiple_boucle.boucle_1()
+    print('-' * 50)
+
+    print('Boucle 2')
+    multiple_boucle.boucle_2()
+    print('-' * 50)
+
+    print('Boucle 3')
+    multiple_boucle.boucle_3()
+
+if __name__ == '__main__':
+    main()
